04f5bc9f7170f999aa21078cd27a008c2961dd65
Yudi Hertanto <y.hertanto17@gmail.com>
2021-06-28 13:35:40 +0700

    add builder

--- src/helper.php
+++ src/helper.php
@@ -14,11 +14,31 @@ if (!function_exists('arr_strict')) {
 }
 
 if (!function_exists('apply_scope')) {
+    /**
+     * The helper for apply scope
+     *
+     * @param string $scope
+     * @return array
+     */
     function apply_scope(string $scope): array
     {
         return [
             $scope,
-            app($scope)
+            \app($scope)
         ];
+    }
+}
+
+
+if (!function_exists('builder_to_sql')) {
+    /**
+     * The helper for build sql query from builder to string
+     *
+     * @param $builder
+     * @return string
+     */
+    function builder_to_sql($builder): string
+    {
+        return vsprintf(str_replace(["?"], ['\'%s\''], $builder->toSql()), $builder->getBindings());
     }
 }
